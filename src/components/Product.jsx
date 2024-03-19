88272a24844c86fb6f12fd539e0d21f4fe2bb6d3
Todd MC Tsai <toddmctsai@gmail.com>
2024-03-19 14:07:00 +0800

    list latest products with img, name, rating, num of reviews, price

---
+++ src/components/Product.jsx
@@ -0,0 +1,44 @@
+// import React from 'react'
+import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+
+function Product({ product }) {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} />
+      </a>
+      
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+
+          <Card.Text as="div">
+            <div className="my-3">
+              {product.rating} from {product.numReviews} reviews
+            </div>
+          </Card.Text>
+
+          <Card.Text as="h3">
+            ${product.price}
+          </Card.Text>
+        </a>
+      </Card.Body>
+    </Card>
+  )
+}
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    numReviews: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Product
