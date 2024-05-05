b73c77cf87eacb4e754c1f4a22f60e436c5c08e1
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-05 17:05:43 +0800

    access URL in .env from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite';
+
 
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react(), reactRefresh(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [react(),reactRefresh(), tsconfigPaths()],
-  
-})
+  });
+};
