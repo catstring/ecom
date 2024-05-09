ac784f0f9afe4a8b35168a3c69222a9ea9e5da02
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-09 22:28:22 +0800

    test screen

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import TestScreen from './screens/TestScreen'
 
 function App() {
 
@@ -18,11 +19,12 @@ function App() {
           <main className='py-3'>
             <Container>
               <Routes>
-                <Route path='/' element={<HomeScreen/>} exact />
+                <Route path='/' element={<TestScreen/>} />
+                {/* <Route path='/' element={<HomeScreen/>} exact />
                 <Route path='/login' element={<LoginScreen/>} />
                 <Route path='/register' element={<RegisterScreen/>} />
                 <Route path='/product/:id' element={<ProductScreen/>} />
-                <Route path='/cart/:id?' element={<CartScreen/>} />
+                <Route path='/cart/:id?' element={<CartScreen/>} /> */}
               </Routes>
             </Container>
           </main>
---
+++ src/screens/TestScreen.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function TestScreen() {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default TestScreen
