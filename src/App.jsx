9c15db871e2255003264fd47e2d83a703dbac9d4
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-10 17:07:19 +0800

    resume routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-// import HomeScreen from './screens/HomeScreen'
-// import ProductScreen from './screens/ProductScreen'
-// import CartScreen from './screens/CartScreen'
-// import LoginScreen from './screens/LoginScreen'
-// import RegisterScreen from './screens/RegisterScreen'
-import TestScreen from './screens/TestScreen'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+// import TestScreen from './screens/TestScreen'
 
 function App() {
 
@@ -18,12 +18,12 @@ function App() {
           <main className='py-3'>
             <Container>
               <Routes>
-                <Route path='/' element={<TestScreen/>} />
-                {/* <Route path='/' element={<HomeScreen/>} exact />
+                {/* <Route path='/' element={<TestScreen/>} /> */}
+                <Route path='/' element={<HomeScreen/>} exact />
                 <Route path='/login' element={<LoginScreen/>} />
                 <Route path='/register' element={<RegisterScreen/>} />
                 <Route path='/product/:id' element={<ProductScreen/>} />
-                <Route path='/cart/:id?' element={<CartScreen/>} /> */}
+                <Route path='/cart/:id?' element={<CartScreen/>} />
               </Routes>
             </Container>
           </main>
