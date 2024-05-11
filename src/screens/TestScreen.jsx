cb88aae8010725e0ad7152509baa8afb6e946839
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-11 15:46:56 +0800

    test

--- src/screens/TestScreen.jsx
+++ src/screens/TestScreen.jsx
@@ -1,9 +1,20 @@
 // import React from 'react'
 
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchProducts } from "../state/productSlice"
+
 function TestScreen() {
+  const product = useSelector((state) => state.product)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      dispatch(fetchProducts())
+  },[dispatch])
+
   return (
     <div>
-      Hello World
+      Test
     </div>
   )
 }
