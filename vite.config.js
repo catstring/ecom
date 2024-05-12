42953b7925ee98a788e92c94b85e48a84a312349
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-12 18:25:37 +0800

    clean up viteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { loadEnv } from 'vite';
-
-
+// import { loadEnv } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  console.log(env.VITE_BASE_URL)
-  return defineConfig({
-    plugins: [react(), reactRefresh(), tsconfigPaths()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_BASE_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  });
-};
+export default defineConfig({
+  plugins: [
+    react(),  // Setup React with fast refresh
+    reactRefresh(),
+    tsconfigPaths()  // Automatically resolves TypeScript path mappings
+  ]
+});
